6ee6b89f643a2c66b596432081de342be65e9d0a
Abdallah Abedraba <aabedraba@gmail.com>
2017-10-24 08:49:37 +0200

    PreEntrega

--- Practica_3/main.cpp
+++ Practica_3/main.cpp
@@ -8,6 +8,7 @@
 #include <cstdlib>
 #include <iostream>
 #include <string>
+#include <sstream>
 
 #include "Diccionario.h"
 
@@ -19,9 +20,35 @@ using namespace std;
 int main(int argc, char** argv) {
 
     try {
-        Diccionario diccionario("listado-sin-acentos_v2.txt");
-        diccionario.usaCorpus("corpus_spanish.txt");
         
+        Diccionario diccionario("listado-sin-acentos_v2.txt");
+        diccionario.usaCorpus("corpus_spanish-2.txt");     
+        std::string palabra;
+        int pos, eleccion;
+        do {
+            cout << "Introduzca una palabra: ";
+            cin >> palabra;    
+            if (palabra == "-1")
+                break;
+            ListaEnlazada<Sucesor>::Iterador iter = diccionario.busca( palabra, pos ).sucesores().iterador();
+            ListaEnlazada<Sucesor>::Iterador aux = iter;
+            if ( iter.fin() ){
+                cout << "No hay sucesores" << endl;
+                continue;
+            }
+            int eleccion, i = 1;
+            cout << "Elija: ";
+            while ( !iter.fin() ){
+                cout << i << ". " << iter.dato().getTermino() << "(" << iter.dato().getNumOcurrencias() << ")  ";
+                iter.siguiente();
+                i++;
+            }
+            cout << "\n: ";
+            cin >> eleccion;
+            for (int i = 2; i <= eleccion; i++)
+                aux.siguiente();
+        } while (true);
+             
     } catch (std::ifstream::failure& exception) {
     std::cerr << exception.what() << std::endl;
     }
@@ -35,6 +62,10 @@ int main(int argc, char** argv) {
       std::cerr << exception.what() << std::endl;
     }
     
+    
+    
     return 0;
 }
+
+
 
