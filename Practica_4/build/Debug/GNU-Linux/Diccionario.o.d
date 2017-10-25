4a0b2cb7f95ac835fc7fe4b15fa69ac5428b1381
Abdallah Abedraba <aabedraba@gmail.com>
2017-10-26 01:24:31 +0200

    Deleting ListaEnalzada.h

---
+++ Practica_4/build/Debug/GNU-Linux/Diccionario.o.d
@@ -0,0 +1,6 @@
+build/Debug/GNU-Linux/Diccionario.o: Diccionario.cpp Diccionario.h \
+ Palabra.h
+
+Diccionario.h:
+
+Palabra.h:
--- Practica_4/build/Debug/GNU-Linux/main.o.d
+++ Practica_4/build/Debug/GNU-Linux/main.o.d
@@ -1,8 +1,5 @@
-build/Debug/GNU-Linux/main.o: main.cpp Diccionario.h Palabra.h \
- ListaEnlazada.h
+build/Debug/GNU-Linux/main.o: main.cpp Diccionario.h Palabra.h
 
 Diccionario.h:
 
 Palabra.h:
-
-ListaEnlazada.h:
