b26a1ccc946b8f115b6183227496aec8cb5a4a25
Abdallah Abedraba <aabedraba@gmail.com>
2017-10-27 01:28:14 +0200

    Big main

--- Practica_4/main.cpp
+++ Practica_4/main.cpp
@@ -24,10 +24,10 @@ void mostrarSucesores ( const std::string &palabra, Diccionario &diccionario ){
         cout << "No hay sucesores" << endl;
         return;
     }
-    cout << "Elija ";
+    cout << "Elija: " << endl;
     int i = 1;
-    while ( iter != sucesores.end() ){
-        cout << i << ". " << (*iter).getTermino() << "(" << (*iter).getNumOcurrencias() << ")  ";
+    while ( i <= 10 && iter != sucesores.end() ){
+        cout << '\t' <<  i << ". " << (*iter).getTermino() << "(" << (*iter).getNumOcurrencias() << ")  " << endl;
         iter++;
         i++;
     }
@@ -47,45 +47,65 @@ int main(int argc, char** argv) {
     try {
         Diccionario diccionario("listado-sin-acentos_v2.txt");
         diccionario.usaCorpus("corpus_spanish.txt"); 
+        unsigned int pos, eleccion;
+        std::string palabra;
         do {
-            std::string palabra;
-
-//            cout << "0 buscar, 1 insertar, 2 eliminar, 3 entrena: ";
-//            cin >> eleccion;
-            
-
-//            if ( eleccion == 0 ){
-//                cout << "Palabra a buscar: ";
-//                cin >> palabra;
-//                diccionario.busca( palabra, pos );
-//                if ( pos == -1 )
-//                    cout << "No se encuentra" << endl;
-//                else 
-//                    cout << "Encontrado en la posicion: " << pos << endl;
-//            }
-//            if ( eleccion == 1 ){
-//                cout << "Palabra a insertar: ";
-//                cin >> palabra;
-//                diccionario.insertar( palabra, pos );
-//                if ( pos == -1 )
-//                    cout << "Ya existe" << endl;
-//                else
-//                    cout << "Insertado en la posicion: " << pos << endl;
-//            }
-//            if ( eleccion == 2 ){
-//                cout << "Palabra a eliminar: ";
-//                cin >> palabra;
-//                diccionario.eliminar( palabra, pos );
-//                cout << "Eliminado correctamente de la posicion " << pos << endl;
-//            }    
-            cout << "Introduzca una palabra (0 para salir): ";
-            cin >> palabra;    
-            if (palabra == "0")
-                break;
-            mostrarSucesores( palabra, diccionario );
-            
-            
-       } while (true);
+            try {              
+                cout << "0 buscar, 1 insertar, 2 eliminar, 3 sucesores, 4 salir: ";
+                cin >> eleccion;
+                switch ( eleccion ){
+                    case 0: {
+                        cout << "Palabra a buscar: ";
+                        cin >> palabra;
+                        diccionario.busca( palabra, pos );
+                        if ( pos == -1 )
+                            cout << "No se encuentra" << endl;
+                        else 
+                            cout << "Encontrado en la posicion: " << pos << endl;
+                        break;
+                    }
+                    
+                    case 1: {
+                        cout << "Palabra a insertar: ";
+                        cin >> palabra;
+                        diccionario.insertar( palabra, pos );
+                        if ( pos == -1 )
+                            cout << "Ya existe" << endl;
+                        else
+                            cout << "Insertado en la posicion: " << pos << endl;
+                        break;
+                    }
+                    
+                    case 2: {
+                        cout << "Palabra a eliminar: ";
+                        cin >> palabra;
+                        diccionario.eliminar( palabra, pos );
+                        cout << "Eliminado correctamente de la posicion " << pos << endl;
+                        break;
+                    }
+                    case 3: {
+                        cout << "Introduzca una palabra: ";
+                        cin >> palabra;    
+                        mostrarSucesores( palabra, diccionario );
+                        break;
+                    }
+                    default:
+                        break;
+                }
+            }catch (std::ifstream::failure& exception) {
+                std::cerr << exception.what() << std::endl;
+            }
+            catch (std::invalid_argument& exception) {
+              std::cerr << exception.what() << std::endl;
+            }
+            catch (std::out_of_range& exception) {
+                std::cerr << exception.what() << std::endl;
+            }
+            catch (std::exception& exception) {
+                std::cerr << exception.what() << std::endl;
+            }         
+        } while ( eleccion != 4 );
+       
     } catch (std::ifstream::failure& exception) {
     std::cerr << exception.what() << std::endl;
     }
