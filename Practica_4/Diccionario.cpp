54341a88a9609fa62a0ac3bffdb9ac660d3b40df
Jesus Pereira <jesuspereira1492@gmail.com>
2017-10-29 12:18:41 +0100

    Añadidos comentarios a ciertos metodos. Revisar

--- Practica_4/Diccionario.cpp
+++ Practica_4/Diccionario.cpp
@@ -7,6 +7,10 @@
 
 #include "Diccionario.h"
 
+/**
+ * Constructor parametrizado de diccionario.
+ * @param nomFich fichero con el que queremos crear nuestro diccionario.
+ */
 Diccionario::Diccionario( const std::string &nomFich )
     : _vec()
 {
@@ -39,8 +43,12 @@ Diccionario::Diccionario( const Diccionario& orig ) {
 Diccionario::~Diccionario() {
 }
 
-
-
+/**
+ * Metodo que busca y elimina una palabra del diccionario a través
+ del atributo de tipo string de cada palabra.
+ * @param palabra string que queremos buscar y eliminar.
+ * @param pos posicion del elemento que queremos borrar y que se actualiza una vez eliminado.
+ */
 void Diccionario::elimina( const std::string &palabra, unsigned int &pos ) {
     Palabra pal( palabra );
     std::vector<Palabra>::iterator iter = _vec.begin();
@@ -55,6 +63,12 @@ void Diccionario::elimina( const std::string &palabra, unsigned int &pos ) {
 }
 
 //TODO mirar por qué me devuelve una pos superior
+/**
+ * Metodo que inserta una palabra en el diccionario a traves de un string.
+ * @param palabra string de la nueva palabra que queremos introducir.
+ * @param pos posicion donde colocaremos la nueva palabra y que se actualizará cuando se haga.
+ * @throw lanza una excepcion de tipo invalid_argument si la palabra ya existe.
+ */
 void Diccionario::inserta(const std::string &palabra, unsigned int& pos) {
     Palabra aInsertar( palabra );
     std::vector<Palabra>::iterator iter = std::lower_bound( _vec.begin(), _vec.end(), aInsertar ); 
@@ -65,8 +79,13 @@ void Diccionario::inserta(const std::string &palabra, unsigned int& pos) {
     iter = std::lower_bound( _vec.begin(), _vec.end(), aInsertar );
 }
 
-
-
+/**
+ * Metodo que busca una palabra en el diccionario a traves de un string.
+ * @param termino string de la palabra que queremos buscar.
+ * @param pos posicion donde se encuentra dicha palabra y que se actualizara cuando se encuentre.
+ * @return devuelve un objeto de tipo palabra con la palabra buscada.
+ * @throw lanza una excepcion de tipo invalid_argument si la palabra no existe.
+ */
 Palabra &Diccionario::busca( const std::string &termino, unsigned int &pos ) {
     Palabra aBuscar( termino );
     auto iter = std::lower_bound( _vec.begin(), _vec.end(), aBuscar );
@@ -77,6 +96,11 @@ Palabra &Diccionario::busca( const std::string &termino, unsigned int &pos ) {
     return (*iter);
 }
 
+/**
+ * Metodo que entrena el diccionario con un archivo repleto de frases.
+ * @param nomFich string con el nombre del fichero que usaremos para entrenar.
+ * @throw excepcion de tipo invalid_argument si el archivo no se puede abrir.
+ */
 void Diccionario::usaCorpus(std::string nomFich) {
     std::ifstream fe;
     std::string frase;
@@ -94,6 +118,11 @@ void Diccionario::usaCorpus(std::string nomFich) {
 }
 
 //TODO preguntar la parte del catch al profesor
+/**
+ * Metodo que a partir de una frase analizará las palabras que contiene y observará 
+ el término que la precede y actualizará con él su lista de sucesores en el diccionario.
+ * @param frase string que contiene la frase que usaremos.
+ */
 void Diccionario::entrena(const std::string frase) {
     std::stringstream ss( frase );
     std::string palabra, sucesor;
