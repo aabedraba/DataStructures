ee5ba02edede246427d7238e3ba8e79361e0cc05
Abdallah Abedraba <aabedraba@gmail.com>
2017-11-07 17:34:15 +0100

    Rotaciones del árbol incluidas

--- Practica_5/AVL.h
+++ Practica_5/AVL.h
@@ -14,14 +14,43 @@
 #ifndef AVL_H
 #define AVL_H
 
+template <typename T>
 class AVL {
 public:
     AVL( );
-    AVL( const AVL& orig );
+    AVL( const AVL<T>& orig );
     virtual ~AVL( );
+    AVL operator=( const AVL<T>& orig );
+    AVL rotIzqda( Nodo<T>* &p );
+    AVL rotDecha( Nodo<T>* &p );
+    
 private:
-
+    
 };
+
+template<typename T>
+void Avl<T>::rotIzqda( Nodo<T>* &p ) {
+    Nodo<T> *q = p, *r;
+    p = r = q->der;
+    q->der = r->izq;
+    r->izq = q;
+    q->bal++;
+    if ( r->bal < 0 ) q->bal += -r->bal;
+    r->bal++;
+    if ( q->bal > 0 ) r->bal += q->bal;
+}
+
+template<typename T>
+void Avl<T>::rotDecha( Nodo<T>* &p ) {
+    Nodo<T> *q = p, *l;
+    p = l = q->izq;
+    q->izq = l->der;
+    l->der = q;
+    q->bal--;
+    if ( l->bal > 0 ) q->bal -= l->bal;
+    l->bal--;
+    if ( q->bal < 0 ) l->bal -= -q->bal;
+}
 
 #endif /* AVL_H */
 
