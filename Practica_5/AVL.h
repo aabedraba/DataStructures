10ec72e4ee5b82ca3aab04636a2952e2259668de
Abdallah Abedraba <aabedraba@gmail.com>
2017-11-07 17:55:43 +0100

    Minimal changes

--- Practica_5/AVL.h
+++ Practica_5/AVL.h
@@ -17,25 +17,24 @@
 template <typename T>
 class Nodo {
 public:
-    Nodo<T> *izq;
-    Nodo<T> *der;
-    T dato;
-    
-    Nodo( ):
-        izq( 0 ),
-        der( 0 )
-    {};
     Nodo( T &ele ):
-        izq( 0 ),
-        der( 0 ),
-        dato( ele )
+        _izq( 0 ),
+        _der( 0 ),
+        _bal ( 0 ),
+        _dato( ele ) 
     {};
     Nodo( const Nodo& orig ):
-        izq( orig.izq ),
-        der( orig.der ),
-        dato( orig.dato )
+        _izq( orig._izq ),
+        _der( orig._der ),
+        _dato( orig._dato ),
+        _bal ( orig._bal )
     {};
     virtual ~Nodo( );
+private:
+    Nodo<T> *_izq;
+    Nodo<T> *_der;
+    T _dato;
+    char _bal;
 };
 
 template <typename T>
@@ -45,35 +44,36 @@ public:
     AVL( const AVL<T>& orig );
     virtual ~AVL( );
     AVL operator=( const AVL<T>& orig );
-    AVL rotIzqda( Nodo<T>* &p );
-    AVL rotDecha( Nodo<T>* &p );
+    void rotIzqda( Nodo<T>* &p );
+    void rotDecha( Nodo<T>* &p );
     
 private:
+    Nodo<T> *raiz;
     
 };
 
 template<typename T>
-void Avl<T>::rotIzqda( Nodo<T>* &p ) {
+void AVL<T>::rotIzqda( Nodo<T>* &p ) {
     Nodo<T> *q = p, *r;
-    p = r = q->der;
-    q->der = r->izq;
-    r->izq = q;
-    q->bal++;
-    if ( r->bal < 0 ) q->bal += -r->bal;
-    r->bal++;
-    if ( q->bal > 0 ) r->bal += q->bal;
+    p = r = q->_der;
+    q->_der = r->_izq;
+    r->_izq = q;
+    q->_bal++;
+    if ( r->_bal < 0 ) q->_bal += -r->_bal;
+    r->_bal++;
+    if ( q->_bal > 0 ) r->_bal += q->_bal;
 }
 
 template<typename T>
-void Avl<T>::rotDecha( Nodo<T>* &p ) {
+void AVL<T>::rotDecha( Nodo<T>* &p ) {
     Nodo<T> *q = p, *l;
-    p = l = q->izq;
-    q->izq = l->der;
-    l->der = q;
-    q->bal--;
-    if ( l->bal > 0 ) q->bal -= l->bal;
-    l->bal--;
-    if ( q->bal < 0 ) l->bal -= -q->bal;
+    p = l = q->_izq;
+    q->_izq = l->_der;
+    l->_der = q;
+    q->_bal--;
+    if ( l->_bal > 0 ) q->_bal -= l->_bal;
+    l->_bal--;
+    if ( q->_bal < 0 ) l->_bal -= -q->_bal;
 }
 
 #endif /* AVL_H */
