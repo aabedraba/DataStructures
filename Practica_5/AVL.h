c7168ea7b057f4c7d3afb45c7eb605d024800283
Jesús <jesuspereira1492@gmail.com>
2017-11-08 21:40:43 +0100

    Añadadidos metodos de las lecciones

--- Practica_5/AVL.h
+++ Practica_5/AVL.h
@@ -44,16 +44,42 @@ public:
     AVL( const AVL<T>& orig );
     AVL operator=( const AVL<T>& orig );
     virtual ~AVL( );
+    bool buscar ( T &ele, T &result );
+    bool insertar(T &ele);
+    void recorreInorden( );
+    unsigned int numElmentos( );
+    unsigned int altura( );
     
 private:
+    void inorden (Nodo<T> *p, int nivel);
     void rotIzqda( Nodo<T>* &p );
     void rotDecha( Nodo<T>* &p );
     int inserta( Nodo<T>* &c, T &dato );
+    Nodo<T>* insertaDato(T &ele, Nodo<T>* &p);
+    Nodo<T>* buscaClave (T &ele, Nodo<T> *p);
     
 private:
-    Nodo<T> *raiz;
+    Nodo<T> *_raiz;
+    unsigned int _numElementos;
     
 };
+
+template <typename T>
+void AVL<T>::inorden (Nodo<T> *p, int nivel){
+    if (p){
+        inorden (p->_izq, nivel+1);
+        // Sustituir por procesamiento ----
+        std::cout << "Procesando nodo " << p->_dato;
+        std::cout << "en el nivel " << nivel << std::endl;
+        // -----------
+        inorden (p->_der, nivel+1);
+    }
+}
+
+template <typename T>
+void AVL<T>::recorreInorden() {
+    inorden ( _raiz, 0 );
+}
 
 template<typename T>
 void AVL<T>::rotIzqda( Nodo<T>* &p ) {
@@ -80,35 +106,93 @@ void AVL<T>::rotDecha( Nodo<T>* &p ) {
 }
 
 template <typename T>
-int AVL<T>::inserta( Nodo<T>* &c, T &dato ) {
+int AVL<T>::insertaDato( Nodo<T>* &c, T &dato ) {
     Nodo<T> *p = c;
     int deltaH = 0;
     if (!p) {
         p = new Nodo<T>(dato);
-        c = p; deltaH=1;
-    } else if (dato > p->dato) {
-        if (inserta(p->der, dato)){
-        p->bal--;
-            if (p->bal == -1) 
+        c = p; 
+        deltaH=1;
+    } else if (dato > p->_dato) {
+        if (inserta(p->_der, dato)){
+            p->_bal--;
+            _numElementos++;
+            if (p->_bal == -1) 
                 deltaH=1;
-            else if (p->bal == -2) {
-                if (p->der->bal == 1) 
-                    rotDecha(p->der);
+            else if (p->_bal == -2) {
+                if (p->_der->_bal == 1) 
+                    rotDecha(p->_der);
                 rotIzqda(c);
             } 
         } 
-    } else if (dato < p->dato) {
-        if (inserta(p->izq, dato)){
-        p->bal++;
-            if (p->bal==1) deltaH = 1;
-            else if (p->bal == 2) {
-                if (p->izq->bal == -1) 
-                    rotIzqda(p->izq);
+    } else if (dato < p->_dato) {
+        if (inserta(p->_izq, dato)){
+        p->_bal++;
+        _numElementos++;
+            if (p->_bal==1) deltaH = 1;
+            else if (p->_bal == 2) {
+                if (p->_izq->_bal == -1) 
+                    rotIzqda(p->_izq);
                 rotDecha(c);
             } 
         } 
     }
     return deltaH;
+}
+
+template <typename T>
+Nodo<T> *AVL<T>::insertaDato(T &ele, Nodo<T>* &p){
+    if (!p) {
+        p = new Nodo<T>(ele);
+    } else {
+        if (ele <= p->_dato)
+            p->_izq = insertaDato(ele, p->_izq);
+        else
+            p->_der = insertaDato(ele, p->_der);
+    }
+    _numElementos++;
+    return p;
+}
+
+template <typename T>
+bool AVL<T>::insertar(T &ele){
+    bool encontrado = buscar(ele);
+    if (!encontrado) {
+        insertaDato(ele, _raiz);
+        return true;
+    }
+    return false;
+}
+
+template <typename T>
+Nodo<T> *AVL<T>::buscaClave (T &ele, Nodo<T> *p){
+    if (!p)
+        return 0;
+    else if (ele < p->_dato)
+        return buscaClave (ele, p->_izq);
+    else if (ele > p-> _dato)
+        return buscaClave (ele, p->_der);
+    else return p;
+}
+
+template <typename T>
+bool AVL<T>::buscar (T &ele, T &result){
+    Nodo<T> *p = buscaClave (ele, _raiz);
+    if (p) {
+        result = p->_dato;
+        return true;
+    }
+    return false;
+}
+
+template <typename T>
+unsigned int AVL<T>::numElmentos() {
+    return _numElementos;
+}
+
+template <typename T>
+unsigned int AVL<T>::altura() {
+
 }
 
 #endif /* AVL_H */
