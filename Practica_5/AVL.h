f3fc98279442d54b7a39573b53a9392632d651e8
Abdallah Abedraba <aabedraba@gmail.com>
2017-11-08 23:21:56 +0100

    Avl semi-perfecto (no Altura)

--- Practica_5/AVL.h
+++ Practica_5/AVL.h
@@ -15,6 +15,10 @@
 #ifndef AVL_H
 #define AVL_H
 
+#include <queue>
+#include <iostream>
+
+
 template <typename T>
 class Nodo {
 public:
@@ -22,41 +26,45 @@ public:
         _izq( 0 ),
         _der( 0 ),
         _bal ( 0 ),
-        _dato( ele ) 
+        _dato( ele ),
+        _altura ( 0 )
     {};
     Nodo( const Nodo& orig ):
         _izq( orig._izq ),
         _der( orig._der ),
         _dato( orig._dato ),
-        _bal ( orig._bal )
+        _bal ( orig._bal ),
+        _altura ( orig._altura )
     {};
-    virtual ~Nodo( );
+    virtual ~Nodo( ) {};
     Nodo<T> *_izq;
     Nodo<T> *_der;
     T _dato;
     char _bal;
+    int _altura;
 };
 
 template <typename T>
-class AVL {
+class Avl {
 public:
-    AVL( );
-    AVL( const AVL<T>& orig );
-    AVL operator=( const AVL<T>& orig );
-    virtual ~AVL( );
-    bool buscar ( T &ele, T &result );
-    bool insertar(T &ele);
+    Avl( );
+    Avl( const Avl<T>& orig );
+    Avl &operator=( const Avl<T>& orig );
+    virtual ~Avl( );
+    bool insertar( T &ele );
+    bool buscar ( T &ele );
     void recorreInorden( );
     unsigned int numElmentos( );
-    unsigned int altura( );
+    unsigned int alturaAvl( );
     
 private:
-    void inorden (Nodo<T> *p, int nivel);
+    void inorden ( Nodo<T> *p, std::queue<T> &cola );
     void rotIzqda( Nodo<T>* &p );
     void rotDecha( Nodo<T>* &p );
-    int inserta( Nodo<T>* &c, T &dato );
-    Nodo<T>* insertaDato(T &ele, Nodo<T>* &p);
+    int insertaDato( Nodo<T>* &c, T &dato );
     Nodo<T>* buscaClave (T &ele, Nodo<T> *p);
+    unsigned int altura( Nodo<T> *n );
+    void borraNodos ( Nodo<T>* &p );
     
 private:
     Nodo<T> *_raiz;
@@ -65,107 +73,140 @@ private:
 };
 
 template <typename T>
-void AVL<T>::inorden (Nodo<T> *p, int nivel){
-    if (p){
-        inorden (p->_izq, nivel+1);
-        // Sustituir por procesamiento ----
-        std::cout << "Procesando nodo " << p->_dato;
-        std::cout << "en el nivel " << nivel << std::endl;
-        // -----------
-        inorden (p->_der, nivel+1);
+Avl<T>::Avl()
+    : _raiz( 0 ),
+      _numElementos( 0 )
+{}
+
+template <typename T>
+Avl<T>::Avl( const Avl<T>& orig ){
+    std::queue<T> cola;
+    inorden( orig._raiz, cola );
+    while( !cola.empty() ){
+        insertar( cola.front() );
+        cola.pop();
     }
 }
 
 template <typename T>
-void AVL<T>::recorreInorden() {
-    inorden ( _raiz, 0 );
+Avl<T> &Avl<T>::operator =(const Avl<T>& orig){
+    std::queue<T> cola;
+    inorden( orig._raiz, cola );
+    while( !cola.empty() ){
+        insertar( cola.front() );
+        cola.pop();
+    }
+}
+
+template <typename T>
+Avl<T>::~Avl(){
+    borraNodos( _raiz );
+}
+
+template <typename T>
+void Avl<T>::borraNodos( Nodo<T>* &p ){
+    if ( p ){
+        borraNodos( p->_izq );
+        borraNodos( p->_der );
+        delete p;
+    }
+}
+
+template <typename T>
+void Avl<T>::inorden ( Nodo<T> *p, std::queue<T> &cola ){
+    if ( p ){
+        inorden ( p->_izq, cola );
+        cola.push( p->_dato );
+        inorden (p->_der, cola );
+    }
+}
+
+template <typename T>
+void Avl<T>::recorreInorden() {
+    std::queue<T> cola;
+    inorden ( _raiz, cola );
+    while( !cola.empty() ){
+        std::cout << cola.front() << ' ';
+        cola.pop();
+    }
+    std::cout << "\n";
 }
 
 template<typename T>
-void AVL<T>::rotIzqda( Nodo<T>* &p ) {
+void Avl<T>::rotIzqda( Nodo<T>* &p ) {
     Nodo<T> *q = p, *r;
     p = r = q->_der;
     q->_der = r->_izq;
     r->_izq = q;
     q->_bal++;
-    if ( r->_bal < 0 ) q->_bal += -r->_bal;
+    if ( r->_bal < 0 ) 
+        q->_bal += -r->_bal;
     r->_bal++;
-    if ( q->_bal > 0 ) r->_bal += q->_bal;
+    if ( q->_bal > 0 ) 
+        r->_bal += q->_bal;
 }
 
 template<typename T>
-void AVL<T>::rotDecha( Nodo<T>* &p ) {
+void Avl<T>::rotDecha( Nodo<T>* &p ) {
     Nodo<T> *q = p, *l;
     p = l = q->_izq;
     q->_izq = l->_der;
     l->_der = q;
     q->_bal--;
-    if ( l->_bal > 0 ) q->_bal -= l->_bal;
+    if ( l->_bal > 0 ) 
+        q->_bal -= l->_bal;
     l->_bal--;
-    if ( q->_bal < 0 ) l->_bal -= -q->_bal;
+    if ( q->_bal < 0 ) 
+        l->_bal -= -q->_bal;
 }
 
 template <typename T>
-int AVL<T>::insertaDato( Nodo<T>* &c, T &dato ) {
+int Avl<T>::insertaDato( Nodo<T>* &c, T &dato  ) {
     Nodo<T> *p = c;
     int deltaH = 0;
     if (!p) {
         p = new Nodo<T>(dato);
-        c = p; 
-        deltaH=1;
+        c = p;
+        deltaH = 1;
     } else if (dato > p->_dato) {
-        if (inserta(p->_der, dato)){
+        if ( insertaDato( p->_der, dato ) ){
             p->_bal--;
-            _numElementos++;
-            if (p->_bal == -1) 
-                deltaH=1;
-            else if (p->_bal == -2) {
-                if (p->_der->_bal == 1) 
-                    rotDecha(p->_der);
-                rotIzqda(c);
+            if ( p->_bal == -1 ) deltaH = 1;
+            else if ( p->_bal == -2 ) {
+                if ( p->_der->_bal == 1 ) 
+                    rotDecha( p->_der );
+                rotIzqda( c );
             } 
         } 
-    } else if (dato < p->_dato) {
-        if (inserta(p->_izq, dato)){
-        p->_bal++;
-        _numElementos++;
-            if (p->_bal==1) deltaH = 1;
-            else if (p->_bal == 2) {
-                if (p->_izq->_bal == -1) 
-                    rotIzqda(p->_izq);
+    } else if ( dato < p->_dato ) {
+        if ( insertaDato( p->_izq, dato ) ){
+            p->_bal++;
+            if ( p->_bal == 1 ) deltaH = 1;
+            else if ( p->_bal == 2 ) {
+                if ( p->_izq->_bal == -1 ) 
+                    rotIzqda( p->_izq );
                 rotDecha(c);
             } 
         } 
-    }
+    } else
+        p->_altura++;
+    
     return deltaH;
 }
 
 template <typename T>
-Nodo<T> *AVL<T>::insertaDato(T &ele, Nodo<T>* &p){
-    if (!p) {
-        p = new Nodo<T>(ele);
-    } else {
-        if (ele <= p->_dato)
-            p->_izq = insertaDato(ele, p->_izq);
-        else
-            p->_der = insertaDato(ele, p->_der);
-    }
-    _numElementos++;
-    return p;
-}
-
-template <typename T>
-bool AVL<T>::insertar(T &ele){
-    bool encontrado = buscar(ele);
-    if (!encontrado) {
-        insertaDato(ele, _raiz);
+bool Avl<T>::insertar( T &ele ){
+    bool encontrado = buscar( ele );
+    if ( !encontrado ) {
+        insertaDato( _raiz, ele );
+        _numElementos++;
         return true;
     }
     return false;
 }
 
 template <typename T>
-Nodo<T> *AVL<T>::buscaClave (T &ele, Nodo<T> *p){
+Nodo<T> *Avl<T>::buscaClave (T &ele, Nodo<T> *p){
     if (!p)
         return 0;
     else if (ele < p->_dato)
@@ -176,23 +217,30 @@ Nodo<T> *AVL<T>::buscaClave (T &ele, Nodo<T> *p){
 }
 
 template <typename T>
-bool AVL<T>::buscar (T &ele, T &result){
-    Nodo<T> *p = buscaClave (ele, _raiz);
-    if (p) {
-        result = p->_dato;
-        return true;
-    }
-    return false;
+bool Avl<T>::buscar ( T &ele ){
+    Nodo<T> *p = buscaClave ( ele, _raiz );
+    return p ? true : false;
 }
 
 template <typename T>
-unsigned int AVL<T>::numElmentos() {
+unsigned int Avl<T>::numElmentos() {
     return _numElementos;
 }
 
 template <typename T>
-unsigned int AVL<T>::altura() {
+unsigned int Avl<T>::alturaAvl(){
+    return altura( _raiz );
+}
 
+
+template <typename T>
+unsigned int Avl<T>::altura( Nodo<T> *n ) {
+    if ( n ){
+        unsigned int altIzq =  _raiz->_izq->_altura;
+        unsigned int altDer =  _raiz->_der->_altura;
+        return ( (altIzq > altDer) ? altIzq : altDer );
+    }
+    return 0;
 }
 
 #endif /* AVL_H */
