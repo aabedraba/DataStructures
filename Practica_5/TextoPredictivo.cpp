5703f785e96e375e5df3463e655bc8813159f6e9
Jesús <jesuspereira1492@gmail.com>
2017-11-11 11:37:27 +0100

    TextoPredictivo.cpp documentado

--- Practica_5/TextoPredictivo.cpp
+++ Practica_5/TextoPredictivo.cpp
@@ -7,21 +7,39 @@
 
 #include "TextoPredictivo.h"
 
+/**
+ * Constructor parametrizado de TextoPredictivo.
+ * @param diccIdioma referencia a un objeto de tipo diccionario.
+ */
 TextoPredictivo::TextoPredictivo( Diccionario &diccIdioma ) 
     : _diccIdioma ( diccIdioma )
 {}
 
+/**
+ * Contructor copia de TextoPredictivo.
+ * @param orig TextoPredictivo que se quiere copiar.
+ */
 TextoPredictivo::TextoPredictivo( const TextoPredictivo& orig ) 
-    : _diccIdioma ( _diccIdioma )
+    : _diccIdioma ( orig._diccIdioma )
 {}
 
 TextoPredictivo::~TextoPredictivo()
 {}
 
+/**
+ * Metodo que dado un termino, devuelve una lista con sus sucesores y otra con el numero de ocurrencias de estos sucesores.
+ * @param termino string del que se quieren obtener sucesores.
+ * @param ocurrencias referencia a lista de enteros que queremos rellenar con el numero de ocurrencias de los sucesores.
+ * @return lista de string con los sucesores de termino.
+ */
 std::list<std::string> TextoPredictivo::sugerencia( const std::string& termino, std::list<int> &ocurrencias ) {
     return _diccIdioma.busca( termino ).sucesores( ocurrencias );
 }
 
+/**
+ * Metodo que llama al entrena de diccionario y realiza su misma funcion.
+ * @param frase string que contiene la frase que se usara.
+ */
 void TextoPredictivo::entrena( std::string& frase ) {
     _diccIdioma.entrena( frase );
 }
