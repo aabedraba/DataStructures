d9ea2739982a29d447ab4d4a02032895176fc0e3
Abdallah Abedraba <aabedraba@gmail.com>
2017-11-20 22:01:41 +0100

    Finalizado

--- Practica_6/Usuario.cpp
+++ Practica_6/Usuario.cpp
@@ -36,11 +36,8 @@ std::string Usuario::getId( ) const {
     return _id;
 }
 
-std::string Usuario::getNombre( ) const {
-    return _nombre;
-}
 
-void Usuario::escribeFrase( std::string &frase ) {
+void Usuario::escribeFrase( std::string frase ) {
     std::stringstream ss( frase );
     std::string palabra, sucesor;
     
@@ -49,7 +46,8 @@ void Usuario::escribeFrase( std::string &frase ) {
         ss >> sucesor;
         bool enDiccionario = _textPred->entrena( palabra, sucesor );
         if ( !enDiccionario ){
-            _miDic.inserta( palabra );
+            Palabra *pal = _miDic.busca( palabra );
+            if ( pal == 0 ) _miDic.inserta( palabra );
             _miDic.busca( palabra )->introducirSucesor( sucesor );
         }
         palabra = sucesor;
@@ -59,29 +57,29 @@ void Usuario::escribeFrase( std::string &frase ) {
 
 std::list<std::string> *Usuario::sugerencia( std::string termino ){
     bool dicBase = true, dicUser = true;
+    
     std::list<std::string> *base = _textPred->sugerencia( termino );
-    Palabra *enUsuario = _miDic.busca( "abdallah" );
+    Palabra *enUsuario = _miDic.busca( termino );
+    if ( base == 0 ) dicBase = false;
     if ( enUsuario == 0 ) dicUser = false;
-    if ( base->empty() ) dicBase = false;
     
     if ( !dicBase && !dicUser ){
         _miDic.inserta( termino );
-        throw std::invalid_argument( "[Usuario::sugerencia] Palabra no existe" );
+        return 0;
     }
     
     if ( dicBase && dicUser ){
         std::list<std::string> *aux = new std::list<std::string>;
-        std::list<std::string> *user = enUsuario->sucesores();
         auto iter = base->begin();
-        for ( int i = 0; i < 5; i++, iter )
+        for ( int i = 0; i < 5 && iter != base->end(); i++, iter++ )
             aux->push_back( (*iter ) );
+        std::list<std::string> *user = enUsuario->sucesores();
         iter = user->begin();
-        for ( int i = 0; i < 5; i++, iter++ )
+        for ( int i = 0; i < 5 && iter != user->end() ; i++, iter++ )
             aux->push_back( ( *iter ) );
         return aux;
     } else if ( dicBase )
-        _textPred->sugerencia( termino );
+        return _textPred->sugerencia( termino );
     else
-        return _miDic.busca( termino )->sucesores();
-    
+        return _miDic.busca( termino )->sucesores();  
 }
