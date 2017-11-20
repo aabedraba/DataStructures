c5a9ee814412b80850966b2abced493c7807a008
Abdallah Abedraba <aabedraba@gmail.com>
2017-11-20 22:02:04 +0100

    Fin

--- Practica_6/main.cpp
+++ Practica_6/main.cpp
@@ -21,17 +21,19 @@ using namespace std;
  * @param palabra string del que se buscan sucesores.
  * @param diccionario objeto de tipo diccionario en el buscamos estas palabras.
  */
-void mostrarSucesores ( const std::string &palabra, TextoPredictivo &predictivo ){
+void mostrarSucesores ( const std::string &palabra, Usuario* user ){
     unsigned int pos;
     int eleccion;
     std::list<int> ocurrencias;
-    std::list<std::string> *sucesores = predictivo.sugerencia( palabra );
-    auto iter = sucesores->begin();
-    auto aux = iter;
-    if ( iter == sucesores->end() ){
+    std::list<std::string> *sucesores;
+    sucesores = user->sugerencia( palabra );
+    if ( sucesores == 0 ){
         cout << "No hay sucesores" << endl;
         return;
     }
+    auto iter = sucesores->begin();
+    auto aux = iter;
+    
     cout << "Elija: " << endl;
     int i = 1;
     while ( iter != sucesores->end() ){
@@ -40,9 +42,13 @@ void mostrarSucesores ( const std::string &palabra, TextoPredictivo &predictivo 
     }
     cout << " " << endl;
     cin >> eleccion;
+    if ( eleccion > sucesores->size() ){
+        std::cout << "Valor incorrecto" << std::endl;
+        return;
+    }
     for (int i = 2; i <= eleccion; i++)
         aux++;
-    mostrarSucesores( (*aux), predictivo );
+    mostrarSucesores( (*aux), user );
 };
 
 
@@ -54,65 +60,16 @@ int main(int argc, char** argv) {
     try {
         Diccionario disBase("listado-sin-acentos_v2.txt");
         TextoPredictivo predictivo( disBase );
-        std::string id = "53597523", nombre = "Abdallah"; 
-        predictivo.nuevoUsuario( id, nombre );
-        Usuario *user = predictivo.getUsuario( id );
-        cout << "Hola, soy: " << user->getNombre() << " y mi id es " << user->getId() << endl;
-        std::string frase = "Hola me llamo Abdallah y tengo 16 añacos";
-        user->escribeFrase( frase );
-        user->sugerencia( "Abdallah" );
-        
-//        unsigned int eleccion;
-//        std::string palabra;     
-//        do {
-//            try {              
-//                cout << "0 buscar, 1 insertar, 2 sucesores, 3 salir: ";
-//                cin >> eleccion;
-//                switch ( eleccion ){
-//                    case 0: {
-//                        cout << "Palabra a buscar: ";
-//                        cin >> palabra;
-//                        Palabra buscado;
-//                        cout << disBase.busca( palabra ).getTermino() << " encontrado" << endl;
-//                        
-//                        break;
-//                    }
-//                    
-//                    case 1: {
-//                        cout << "Palabra a insertar: ";
-//                        cin >> palabra;
-//                        bool insertado;
-//                        disBase.inserta( palabra, insertado );
-//                        if ( insertado )
-//                            cout << "Insertado" << endl;
-//                        else
-//                            cout << "Ya existe" << endl;
-//                        break;
-//                    }
-//                    
-//                    case 2: {
-//                        cout << "Introduzca una palabra: ";
-//                        cin >> palabra;    
-//                        mostrarSucesores( palabra, predictivo );
-//                        break;
-//                    }
-//                    default:
-//                        break;
-//                }
-//            }catch (std::ifstream::failure& exception) {
-//                std::cerr << exception.what() << std::endl;
-//            }
-//            catch (std::invalid_argument& exception) {
-//              std::cerr << exception.what() << std::endl;
-//            }
-//            catch (std::out_of_range& exception) {
-//                std::cerr << exception.what() << std::endl;
-//            }
-//            catch (std::exception& exception) {
-//                std::cerr << exception.what() << std::endl;
-//            }         
-//        } while ( eleccion != 3 );
-        
+        predictivo.nuevoUsuario( "usr1", "Abdallah" );
+        predictivo.nuevoUsuario( "usr2", "Jesus" );
+        Usuario *us1 = predictivo.getUsuario( "usr1" );
+        Usuario *us2 = predictivo.getUsuario( "usr2" );
+        us1->escribeFrase( "Hola colega como andas Yo regu porque yo estoy pillao con las prácticas de estructuras" );
+        us2->escribeFrase( "colega el whatsapp el la tuit el pillao" );
+        cout << "Mostrando sucesores  de: " << us1->getId() << endl;
+        mostrarSucesores( "Hola", us1 ); 
+        cout << "Mostrando sucesores  de: " << us2->getId() << endl;
+        mostrarSucesores( "whatsapp", us2 );
     } catch (std::ifstream::failure& exception) {
     std::cerr << exception.what() << std::endl;
     }
