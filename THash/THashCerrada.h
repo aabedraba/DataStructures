ec27b6b1e26e93785bef9f41df09e0234f4eca6f
Abdallah Abedraba <aabedraba@gmail.com>
2017-12-07 21:47:19 +0100

    Pruebas tabla hash

--- THash/THashCerrada.h
+++ THash/THashCerrada.h
@@ -19,37 +19,39 @@
 #include <optional>
 
 #define MAX_COLISIONES 500
-static long posPorDefecto = 0;
+static unsigned long porDefecto = 0;
 
 
 template <typename T>
 class THashCerrada {
 public:
-    explicit THashCerrada( long &tamTabla );
+    explicit THashCerrada( unsigned long &tamTabla );
     THashCerrada( const THashCerrada& orig );
     virtual ~THashCerrada( ) = default;
 
     unsigned long toDjb2( const char *str );
-    unsigned long calculaPrimo( long tamTabla);
+    unsigned long calculaPrimo(unsigned long tamTabla);
 
-    bool insertar( long clave, const T& dato );
-    bool borrar( long clave, const T& dato );
-    T* buscar( long clave, const T& dato, long &posEnVector = posPorDefecto );
+    unsigned long dispersionCuadratica( long clave, const T& dato, unsigned long &colisiones = porDefecto );
+    unsigned long dispersionDoble( long clave, const T& dato, unsigned long &colisiones = porDefecto);
+    bool inserta(long clave, const T &dato);
+    bool borra(long clave, const T &dato);
+    T* busca(long clave, const T &dato, unsigned long &posEnVector = porDefecto);
     unsigned long tamaTabla();
     unsigned long numElementos();
-    unsigned int maxColisiones();
-    unsigned int promedioColisiones();
+    unsigned long maxColisiones();
+    unsigned long promedioColisiones();
     float factorCarga();
 
 private:
     std::vector<std::pair<bool, T>> _tabla;
     unsigned long _numElementos;
-    unsigned int _maxColisiones;
-    unsigned int _promedioColisiones;
+    unsigned long _maxColisiones;
+    unsigned long _promedioColisiones;
 };
 
 template <typename T>
-THashCerrada<T>::THashCerrada( long &tamTabla )
+THashCerrada<T>::THashCerrada(unsigned long &tamTabla )
     : _tabla( calculaPrimo(tamTabla) ),
       _numElementos( 0 ),
       _maxColisiones( 0 ),
@@ -66,7 +68,7 @@ THashCerrada<T>::THashCerrada( const THashCerrada& orig )
 
 
 template<typename T>
-unsigned long THashCerrada<T>::calculaPrimo( long tamTabla ) {
+unsigned long THashCerrada<T>::calculaPrimo( unsigned long tamTabla ) {
     long primo;
     if ( tamTabla % 2 == 0 )
         primo = tamTabla + 1;
@@ -76,13 +78,12 @@ unsigned long THashCerrada<T>::calculaPrimo( long tamTabla ) {
     bool noEsPrimo;
     do {
         noEsPrimo = false;
-        for ( int i = 2, x = std::sqrt( primo ); i <= x; i++ ){
+        for ( long i = 2, x = std::sqrt( primo ); i <= x; i++ )
             if ( primo % i == 0 ){
                 noEsPrimo = true;
                 primo += 2;
                 break;
             }
-        }
     } while ( noEsPrimo );
     return primo;
 }
@@ -91,53 +92,75 @@ unsigned long THashCerrada<T>::calculaPrimo( long tamTabla ) {
 template <typename T>
 unsigned long THashCerrada<T>::toDjb2( const char *str ) {
     unsigned long hash = 5381;
-    int c;
+    long c;
     while ( c = *str++ )
         hash = ( (hash << 5) + hash ) + c;
     return hash;
 }
 
-
-template<typename T>
-bool THashCerrada<T>::insertar( long clave, const T& dato ){
+template <typename T>
+unsigned long THashCerrada<T>::dispersionCuadratica(long clave, const T &dato, unsigned long &colisiones) {
     long posicion;
-    int i;
-    for (i = 0; i <= MAX_COLISIONES; ++i) {
+    for (long i = 0; i < MAX_COLISIONES; ++i) {
         posicion = (clave + i*i) % _tabla.size();
-        if ( _tabla[posicion].first == true && _tabla[posicion].second == dato ) return false;
+        if ( _tabla[posicion].first == true && _tabla[posicion].second == dato ) {
+            colisiones = i;
+            return posicion;
+        }
         if ( _tabla[posicion].first == false ) {
-            _tabla[posicion].first = true;
-            _tabla[posicion].second = dato;
-            ++_numElementos;
-            break;
+            colisiones = i;
+            return posicion;
         }
     }
-    _promedioColisiones = (_promedioColisiones + i) / 2;
-    if ( i > _maxColisiones ) _maxColisiones = i;
+}
+
+template <typename T>
+unsigned long THashCerrada<T>::dispersionDoble(long clave, const T &dato, unsigned long &colisiones) {
+    long posicion;
+    long primerPrimo = calculaPrimo(_tabla.size()-1000);
+    long segundoPrimo = calculaPrimo(_tabla.size()-500);
+    long primerHash = clave % primerPrimo;
+    long segundoHash = clave % segundoPrimo;
+    for (long i = 0; i < MAX_COLISIONES; ++i) {
+        posicion = (primerHash + i*segundoHash) % _tabla.size();
+        if ( _tabla[posicion].first == true && _tabla[posicion].second == dato ) {
+            colisiones = i;
+            return posicion;
+        }
+        if ( _tabla[posicion].first == false ) {
+            colisiones = i;
+            return posicion;
+        }
+    }
+}
+
+template<typename T>
+bool THashCerrada<T>::inserta(long clave, const T &dato){
+    unsigned long colisiones;
+    unsigned long posicion = dispersionDoble(clave, dato, colisiones);
+    if ( _tabla[posicion].first == true ) return false; //ya está insertado
+    _tabla[posicion].first = true;
+    _tabla[posicion].second = dato;
+    ++_numElementos;
+    _promedioColisiones = (_promedioColisiones + colisiones) / 2;
+    if ( colisiones > _maxColisiones ) _maxColisiones = colisiones;
     return true;
 }
 
 
 template<typename T>
-T* THashCerrada<T>::buscar(long clave, const T &dato, long &posEnVector) {
-    long posicion;
-    for (int i = 0; i <= MAX_COLISIONES; ++i) {
-        posicion = (clave + i*i) % _tabla.size();
-        if ( _tabla[posicion].first == true )
-            if ( _tabla[posicion].second == dato ) {
-                posEnVector = posicion;
-                return &_tabla[posicion].second;
-            }
-    }
-    return nullptr;
+T* THashCerrada<T>::busca(long clave, const T &dato, unsigned long &posEnVector) {
+    posEnVector = dispersionDoble(clave, dato);
+    if ( _tabla[posEnVector].first == false ) return nullptr;
+    else return &_tabla[posEnVector].second;
 }
 
 template <typename T>
-bool THashCerrada<T>::borrar(long clave, const T &dato) {
-    long posEnVector;
-    T* aBuscar = buscar( clave, dato, posEnVector );
+bool THashCerrada<T>::borra(long clave, const T &dato) {
+    unsigned long posicion;
+    T* aBuscar = busca(clave, dato, posicion);
     if ( aBuscar != nullptr ) {
-        _tabla[posEnVector].first = false;
+        _tabla[posicion].first = false;
         _numElementos--;
         return true;
     }
@@ -155,12 +178,12 @@ unsigned long THashCerrada<T>::numElementos() {
 }
 
 template <typename T>
-unsigned int THashCerrada<T>::maxColisiones() {
+unsigned long THashCerrada<T>::maxColisiones() {
     return _maxColisiones;
 }
 
 template <typename T>
-unsigned int THashCerrada<T>::promedioColisiones() {
+unsigned long THashCerrada<T>::promedioColisiones() {
     return _promedioColisiones;
 }
 
--- THash/main.cpp
+++ THash/main.cpp
@@ -7,30 +7,32 @@
 using namespace std;
 
 int main() {
-    long tam = 120000;
-    int *hola = new int(5);
-    cout << *hola << endl;
-    THashCerrada<Palabra> palabras( tam );
-    ifstream archivo;
-    archivo.open("/home/aabedraba/Github/EstructurasDeDatos/Practica_7/listado-sin-acentos_v2.txt");
-    if ( !archivo.good() ){
-        cout << "No se ha abierto el archivo" << endl;
-        return 0;
-    }
-    string linea;
-    while ( !archivo.eof() ){
-        archivo >> linea;
-        unsigned long clave = palabras.toDjb2( linea.c_str() );
-        Palabra palabra( linea );
-        palabras.insertar( clave, palabra );
-    }
+    unsigned long tam = 125182;
+    int iteraciones = 0;
+//    while (iteraciones < 20){
+        THashCerrada<Palabra> palabras(tam);
+        ifstream archivo;
+        archivo.open("/home/aabedraba/Github/EstructurasDeDatos/Practica_7/listado-sin-acentos_v2.txt");
+        if (!archivo.good()) {
+            cout << "No se ha abierto el archivo" << endl;
+            return 0;
+        }
+        string linea;
+        while (!archivo.eof()) {
+            archivo >> linea;
+            unsigned long clave = palabras.toDjb2(linea.c_str());
+            Palabra palabra(linea);
+            palabras.inserta(clave, palabra);
+        }
 
-    cout << "Tamaño de la tabla: " << palabras.tamaTabla() << endl;
-    cout << "Numero de elementos; " << palabras.numElementos() << endl;
-    cout << "Numero máximo de colisiones: " << palabras.maxColisiones() << endl;
-    cout << "Promedio de colisiones: " << palabras.promedioColisiones() << endl;
-    cout << "Factor de caraga: " << palabras.factorCarga() << endl;
+        cout << "Tamaño de la tabla: " << palabras.tamaTabla() << endl;
+        cout << "Numero de elementos; " << palabras.numElementos() << endl;
+        cout << "Numero máximo de colisiones: " << palabras.maxColisiones() << endl;
+        cout << "Promedio de colisiones: " << palabras.promedioColisiones() << endl;
+        cout << "Factor de caraga: " << palabras.factorCarga() << endl;
+//        tam = palabras.tamaTabla();
+//        iteraciones++;
+//    }
 
-    delete hola;
     return 0;
 }
