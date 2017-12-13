f3d665d8639a705231829fb377c13656618d9d13
Abdallah Abedraba <aabedraba@gmail.com>
2017-12-13 20:23:14 +0100

    Comienzo de práctica

--- Practica_8/main.cpp
+++ Practica_8/main.cpp
@@ -1,100 +1,164 @@
-/* 126150
- * File:   main.cpp
- * Author: aabedraba
- *
- * Created on September 19, 2017, 9:55 AM
- */
+//
+//  main.cpp
+//  ImgQuad
+//
+//  Created by Antonio Jesús Rueda Ruiz on 09/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
 
-#include <cstdlib>
+#include <fstream>
 #include <iostream>
-#include <string>
-#include <sstream>
-#include <chrono>
+#include <vector>
+#include <cstring>
+#include "img.h"
 
-#include "Diccionario.h"
-#include "TextoPredictivo.h"
+#include <math.h>
 
 using namespace std;
 
-/**
- * Funcion recursiva que muestra los 10 sucesores mas prioritarios, en caso de 
- que existan de un string dado.
- * @param palabra string del que se buscan sucesores.
- * @param diccionario objeto de tipo diccionario en el buscamos estas palabras.
- */
-void mostrarSucesores ( const std::string &palabra, Usuario* user ){
-    std::list<std::string> sucesores;
-    try {
-        sucesores = user->sugerencia( palabra );
-    } catch ( logic_error &e ) {
-        cerr << "No hay más sugerencias" << endl;
-        return;
-    }
-    auto iter = sucesores.begin();
-    auto aux = iter;
-
-    cout << "Elija: " << endl;
-    int i = 1;
-    while ( iter != sucesores.end() ){
-        cout << '\t' <<  i << ". " << (*iter) << endl;
-        iter++; i++;
-    }
-
-
-    int eleccion;
-    cout << " " << endl;
-    cin >> eleccion;
-    if ( eleccion > sucesores.size() ){
-        cerr << "Valor incorrecto" << std::endl;
-        return;
-    }
-    for (i = 2; i <= eleccion; i++)
-        aux++;
-    mostrarSucesores( (*aux), user );
-
-};
-
-
-/*
- * 
- */
-int main(int argc, char** argv) {
+int main(int argc, const char * argv[]) {
+    Img img(1, 1);
 
     try {
-        auto start = std::chrono::system_clock::now();
-        unsigned long tam = 126150;
-        Diccionario dicBase("/home/aabedraba/Github/EstructurasDeDatos/Practica_7/listado-sin-acentos_v2.txt", tam);
-        dicBase.usaCorpus("/home/aabedraba/Github/EstructurasDeDatos/Practica_7/corpus_spanish.txt");
-        auto end = std::chrono::system_clock::now();
-        auto elapsed_ms = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-        cout << "Tiempo de carga y entrenamiento del diccionario (segundos): " << elapsed_ms/(float)1000 << endl;
-
-        TextoPredictivo predictivo( dicBase );
-        predictivo.nuevoUsuario( "usr1", "Abdallah" );
-        predictivo.nuevoUsuario( "usr2", "Jesus" );
-        Usuario *us1 = predictivo.getUsuario( "usr1" );
-        Usuario *us2 = predictivo.getUsuario( "usr2" );
-        us1->escribeFrase( "hola colega como andas Yo regu porque yo estoy pillao con las prácticas de estructuras" );
-        us2->escribeFrase( "colega el whatsapp el la tuit el pillao" );
-        cout << "Mostrando sucesores  de: " << us1->getId() << endl;
-        mostrarSucesores( "hola", us1 );
-        cout << "Mostrando sucesores  de: " << us2->getId() << endl;
-        mostrarSucesores( "whatsapp", us2 );
-    } catch (std::ifstream::failure& exception) {
-    std::cerr << exception.what() << std::endl;
-    }
-    catch (std::invalid_argument& exception) {
-      std::cerr << exception.what() << std::endl;
-    }
-    catch (std::out_of_range& exception) {
-      std::cerr << exception.what() << std::endl;
-    }
-    catch (std::exception& exception) {
-      std::cerr << exception.what() << std::endl;
+        img = Img("./mapaUsa.ppm");
+    } catch (ErrorLecturaFichero &e) {
+        cout << "Error al leer imagen de entrada" << endl;
+        return 1;
     }
 
-    return 0;
+    cout << "Imagen leida" << endl;
+
+    int nfilas = img.numFilas();
+    int ncol = img.numColumnas();
+
+    // blue
+    int r = 0;
+    int g = 0;
+    int b = 255;
+
+    double minlat = 9e99;
+    double maxlat = 0;
+    double minlon = 9e99;
+    double maxlon = 0;
+
+    vector<double> vlat, vlon;
+
+    fstream fs("./zipcode.csv", fstream::in | fstream::out);
+    int nlineas = 0;
+    while (!fs.eof()) {
+        char linea[250];
+
+        fs.getline(linea, 250); //la primera no la cuento
+        fs.getline(linea, 250);
+        nlineas++;
+
+        string lin(linea);
+        double numlat, numlon;
+
+        size_t poscoma = lin.find(",", 0);
+        string codigo = lin.substr(0, poscoma);
+        if (codigo.size() > 2) codigo = codigo.substr(1, poscoma - 2);
+        //cout << codigo << ", ";
+        lin = lin.substr(poscoma + 1, 255);
+
+        poscoma = lin.find(",", 0);
+        string city = lin.substr(0, poscoma);
+        if (city.size() > 2) city = city.substr(1, poscoma - 2);
+        //cout << city << ", ";
+        lin = lin.substr(poscoma + 1, 255);
+
+        poscoma = lin.find(",", 0);
+        string state = lin.substr(0, poscoma);
+        if (state.size() > 2) state = state.substr(1, poscoma - 2);
+        //cout << state << ", ";
+        lin = lin.substr(poscoma + 1, 255);
+
+        poscoma = lin.find(",", 0);
+        string lon = lin.substr(0, poscoma);
+        if (lon.size() > 2) {
+            lon.erase(0, 1);
+            lon.erase(lon.size() - 1);
+            char * cslon = new char [lon.length() + 1];
+            strcpy(cslon, lon.c_str());
+            numlon = atof(cslon);
+            vlon.push_back(numlon);
+            //cout << numlon << endl;
+            if (numlon < minlon) minlon = numlon;
+            if (numlon > maxlon) maxlon = numlon;
+
+        }
+        lin = lin.substr(poscoma + 1, 255);
+        
+        poscoma = lin.find(",", 0);
+        string lat = lin.substr(0, poscoma);
+        if (lat.size() > 2) {
+            // Borramos las comillas
+            lat.erase(0, 1);
+            lat.erase(lat.size() - 1);
+            char * cslat = new char [lat.length() + 1];
+            strcpy(cslat, lat.c_str());
+            numlat = atof(cslat);
+            vlat.push_back(numlat);
+            //cout << cslat << ", ";
+            if (numlat < minlat) minlat = numlat;
+            if (numlat > maxlat) maxlat = numlat;
+
+        }
+        lin = lin.substr(poscoma + 1, 255);
+
+    } // fin fichero
+
+
+    cout << "lat: xmin = " << minlat << ", " << "xmax = " << maxlat << endl;
+    cout << "lon: ymin = " << minlon << ", " << "ymax = " << maxlon << endl;
+
+    double rangox = maxlat - minlat;
+    double rangoy = maxlon - minlon;
+
+    cout << "rango x = " << rangox << endl;
+    cout << "rango y = " << rangoy << endl;
+
+    cout << "nfilas = " << nfilas << endl;
+    cout << "ncol = " << ncol << endl;
+
+    double pixelPorGradoX = (double) (nfilas - 1) / rangox;
+    double pixelPorGradoY = (double) (ncol - 1) / rangoy;
+
+    cout << vlon.size() << endl;
+    for (int i = 0; i < vlon.size(); i++) {
+        int posX = (vlat[i] - minlat) * pixelPorGradoX;
+        int posY = ncol - 1 - ((vlon[i] - minlon) * pixelPorGradoY);
+        //img.pintarPixelGrande(posX,posY,r,g,b);
+        img.pintarPixel(posX, posY, r, g, b);
+    }
+
+    img.pintarPixelGrande(0, 0, r, g, b);
+
+
+    double lat1 = -120.0;
+    double lon1 = 43.0;
+
+    double lat2 = -110.0;
+    double lon2 = 44.0;
+
+    img.pintarRecuadro((lat1 - minlat) * pixelPorGradoX, ncol - 1 - ((lon1 - minlon) * pixelPorGradoY),
+            (lat2 - minlat) * pixelPorGradoX, ncol - 1 - ((lon2 - minlon) * pixelPorGradoY), 255, 0, 0);
+
+    // aqui se realiza el test del modo:
+    // vector<CodigoPost> buscarRango (lat1, lon1, lat2, lon2);
+
+    // luego se recorre dicho vector, y para las coordenadas de cada CodigoPost se vuelven a pintar
+    //fichero en color verde
+
+    try {
+        img.guardar("./mapaUsaResult.ppm");
+    } catch (ErrorEscrituraFichero &e) {
+        cout << "Error al escribir fichero" << endl;
+        return 1;
+    }
+
+    cout << "Operación realizada con exito" << endl;
+
+    return EXIT_SUCCESS;
 }
-
-
 
