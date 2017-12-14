a87f355c202e539460e1f17af479053b0152f11d
Abdallah Abedraba <aabedraba@gmail.com>
2017-12-14 19:10:15 +0100

    Añadidas nuevas clases MallaRegular y Casilla

---
+++ Practica_8/Casilla.h
@@ -0,0 +1,59 @@
+//
+// Created by aabedraba on 14/12/17.
+//
+
+#ifndef PRACTICA_8_CASILLA_H
+#define PRACTICA_8_CASILLA_H
+
+#include <list>
+#include "MallaRegular.h"
+
+template <typename T>
+class Casilla {
+public:
+    friend class MallaRegular<T>;
+    Casilla();
+    Casilla( const Casilla& orig ) = default;
+    ~Casilla() = default;
+
+    void insertar( const T &dato );
+    T *buscar( const T &dato );
+    bool borrar( const T &dato );
+
+private:
+    std::list<T> _puntos;
+};
+
+template <typename T>
+Casilla<T>::Casilla()
+    : _puntos()
+{}
+
+template <typename T>
+void Casilla<T>::insertar(const T &dato) {
+    _puntos.push_back( dato );
+}
+
+template <typename T>
+T* Casilla<T>::buscar(const T &dato) {
+    std::list<T>::iterator iter;
+    iter = _puntos.begin();
+    for (; iter != _puntos.end(); ++iter)
+        if ( *iter == dato )
+            return &(*iter);
+    return 0;
+}
+
+template <typename T>
+bool Casilla<T>::borrar(const T &dato) {
+    std::list<T>::iterator iter;
+    iter = _puntos.begin();
+    for ( ; iter != _puntos.end() ; ++iter)
+        if ( *iter == dato ){
+            _puntos.erase(iter);
+            return true;
+        }
+    return false;
+}
+
+#endif //PRACTICA_8_CASILLA_H
---
+++ Practica_8/MallaRegular.h
@@ -0,0 +1,65 @@
+//
+// Created by aabedraba on 14/12/17.
+//
+
+#ifndef PRACTICA_8_MALLAREGULAR_H
+#define PRACTICA_8_MALLAREGULAR_H
+
+#include "Casilla.h"
+#include <vector>
+
+template <typename T>
+class MallaRegular {
+public:
+    MallaRegular( int XMin,int YMin, int XMax, int YMax, int aNDiv );
+    MallaRegular( const MallaRegular& orig ) = default;
+    ~MallaRegular() = default;
+
+    void insertar(float x, float y, const T &dato);
+    T *buscar(float x, float y, const T &dato);
+    bool borrar(float x, float y, const T &dato);
+
+    std::vector<T> buscarRengo( float rxmin, float rymin, float rxmax, float rymax );
+    unsigned numElementos();
+    unsigned maxElementosEnCelda();
+    float promedioElementosPorCelda();
+
+private:
+    Casilla<T> *obtenerCasilla(float x, float y);
+
+private:
+    float _xMin, _yMin, _xMax, _yMax; ///Tamañoo real global
+    float _tamaCasillaX, _tamaCasillaY; ///Tamaño real de cada casilla
+
+    std::vector<std::vector<Casilla<T> > > _mr; ///Vector 2D de casillas
+};
+
+template <typename T>
+MallaRegular<T>::MallaRegular(int XMin, int YMin, int XMax, int YMax, int aNDiv)
+    : _xMin ( XMin ), _yMin ( YMin ), _xMax ( XMax ), _yMax ( YMax ) {
+    _tamaCasillaX = ( _xMax - _xMin ) / aNDiv;
+    _tamaCasillaY = ( _yMax - _yMin ) / aNDiv;
+    _mr.insert( _mr.begin(), aNDiv, std::vector<Casilla<T> >(aNDiv));
+}
+
+
+template<typename T>
+Casilla<T> *MallaRegular<T>::obtenerCasilla (float x, float y){
+    int i = ( x - _xMin ) / _tamaCasillaX;
+    int j = ( y - _yMin ) / _tamaCasillaY;
+    return &_mr[j][i];
+}
+
+template<typename T>
+void MallaRegular<T>::insertar(float x, float y, const T& dato){
+    Casilla<T> *c = obtenerCasilla( x,y );
+    c->insertar( dato );
+}
+
+template<typename T>
+bool MallaRegular<T>::borrar(float x, float y, const T& dato) {
+    Casilla<T> *c = obtenerCasilla( x, y );
+    return c->borrar( dato );
+}
+
+#endif //PRACTICA_8_MALLAREGULAR_H
