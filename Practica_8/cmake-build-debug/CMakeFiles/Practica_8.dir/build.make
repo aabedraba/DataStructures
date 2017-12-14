45934c191fe0fa3c43be769f77e714191dfa9f13
Abdallah Abedraba <aabedraba@gmail.com>
2017-12-15 00:20:37 +0100

    Incluida lectura del archivo zipcode.csv

--- Practica_8/cmake-build-debug/CMakeFiles/Practica_8.dir/build.make
+++ Practica_8/cmake-build-debug/CMakeFiles/Practica_8.dir/build.make
@@ -106,17 +106,17 @@ CMakeFiles/Practica_8.dir/main.cpp.o.provides.build: CMakeFiles/Practica_8.dir/m
 
 
 CMakeFiles/Practica_8.dir/CodigoPost.cpp.o: CMakeFiles/Practica_8.dir/flags.make
-CMakeFiles/Practica_8.dir/CodigoPost.cpp.o: CodigoPost.cpp
+CMakeFiles/Practica_8.dir/CodigoPost.cpp.o: ../CodigoPost.cpp
 	@$(CMAKE_COMMAND) -E cmake_echo_color --switch=$(COLOR) --green --progress-dir=/home/aabedraba/Github/EstructurasDeDatos/Practica_8/cmake-build-debug/CMakeFiles --progress-num=$(CMAKE_PROGRESS_3) "Building CXX object CMakeFiles/Practica_8.dir/CodigoPost.cpp.o"
-	/usr/bin/c++  $(CXX_DEFINES) $(CXX_INCLUDES) $(CXX_FLAGS) -o CMakeFiles/Practica_8.dir/CodigoPost.cpp.o -c /home/aabedraba/Github/EstructurasDeDatos/Practica_8/cmake-build-debug/CodigoPost.cpp
+	/usr/bin/c++  $(CXX_DEFINES) $(CXX_INCLUDES) $(CXX_FLAGS) -o CMakeFiles/Practica_8.dir/CodigoPost.cpp.o -c /home/aabedraba/Github/EstructurasDeDatos/Practica_8/CodigoPost.cpp
 
 CMakeFiles/Practica_8.dir/CodigoPost.cpp.i: cmake_force
 	@$(CMAKE_COMMAND) -E cmake_echo_color --switch=$(COLOR) --green "Preprocessing CXX source to CMakeFiles/Practica_8.dir/CodigoPost.cpp.i"
-	/usr/bin/c++ $(CXX_DEFINES) $(CXX_INCLUDES) $(CXX_FLAGS) -E /home/aabedraba/Github/EstructurasDeDatos/Practica_8/cmake-build-debug/CodigoPost.cpp > CMakeFiles/Practica_8.dir/CodigoPost.cpp.i
+	/usr/bin/c++ $(CXX_DEFINES) $(CXX_INCLUDES) $(CXX_FLAGS) -E /home/aabedraba/Github/EstructurasDeDatos/Practica_8/CodigoPost.cpp > CMakeFiles/Practica_8.dir/CodigoPost.cpp.i
 
 CMakeFiles/Practica_8.dir/CodigoPost.cpp.s: cmake_force
 	@$(CMAKE_COMMAND) -E cmake_echo_color --switch=$(COLOR) --green "Compiling CXX source to assembly CMakeFiles/Practica_8.dir/CodigoPost.cpp.s"
-	/usr/bin/c++ $(CXX_DEFINES) $(CXX_INCLUDES) $(CXX_FLAGS) -S /home/aabedraba/Github/EstructurasDeDatos/Practica_8/cmake-build-debug/CodigoPost.cpp -o CMakeFiles/Practica_8.dir/CodigoPost.cpp.s
+	/usr/bin/c++ $(CXX_DEFINES) $(CXX_INCLUDES) $(CXX_FLAGS) -S /home/aabedraba/Github/EstructurasDeDatos/Practica_8/CodigoPost.cpp -o CMakeFiles/Practica_8.dir/CodigoPost.cpp.s
 
 CMakeFiles/Practica_8.dir/CodigoPost.cpp.o.requires:
 
--- Practica_8/main.cpp
+++ Practica_8/main.cpp
@@ -7,15 +7,107 @@
 //
 
 #include <iostream>
-#include "MallaRegular.h"
+#include <fstream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include "CodigoPost.h"
+
 
 using namespace std;
 
 int main(int argc, const char * argv[]) {
-    MallaRegular<int> mapa( 0, 0, 10, 10, 10 );
-    int num = 5;
-    mapa.insertar( 1, 2, num );
-    mapa.buscar( 1, 2, num );
+    vector<CodigoPost> codigosPostales;
+    ifstream archivo;
+    archivo.open("/home/aabedraba/Github/EstructurasDeDatos/Practica_8/zipcode.csv");
+    if ( !archivo.good() ){
+        cout << "te follen" << endl;
+        return 1;
+    }
+    string linea;
+
+    int codigoPostal;
+    string ciudad, estado;
+    float latitud, longitud;
+    short zonaHoraria;
+    short dst;
+
+
+    string palabra;
+    getline(archivo, linea);
+    int i = 0;
+    while( !archivo.eof() ) {
+        int tamanioLinea;
+        getline(archivo, linea);
+
+        //codigoPostal
+        tamanioLinea = linea.find(",", 0);
+        if ( tamanioLinea == -1 ) continue;
+        palabra = linea.substr(1, tamanioLinea - 2);
+        codigoPostal = stoi(palabra);
+        linea = linea.substr(tamanioLinea+1, 255);
+
+        //ciudad
+        tamanioLinea = linea.find(",", 0);
+        palabra = linea.substr( 1, tamanioLinea-2);
+        ciudad = palabra;
+        linea = linea.substr(tamanioLinea+1, 255);
+
+        //estado
+        tamanioLinea = linea.find(",", 0);
+        palabra = linea.substr( 1, tamanioLinea-2);
+        estado = palabra;
+        linea = linea.substr(tamanioLinea+1, 255);
+
+        //latitud
+        tamanioLinea = linea.find(",", 0);
+        palabra = linea.substr( 1, tamanioLinea-2);
+        latitud = stof(palabra);
+        linea = linea.substr(tamanioLinea+1, 255);
+
+        //longitud
+        tamanioLinea = linea.find(",", 0);
+        palabra = linea.substr( 1, tamanioLinea-2);
+        longitud = stof(palabra);
+        linea = linea.substr(tamanioLinea+1, 255);
+
+        //zonaHoraria
+        tamanioLinea = linea.find(",", 0);
+        palabra = linea.substr( 1, tamanioLinea-2);
+        zonaHoraria = stoi(palabra);
+        linea = linea.substr(tamanioLinea+1, 255);
+
+        //dst
+        tamanioLinea = linea.find("\"", 0);
+        palabra = linea.substr( 1, tamanioLinea-1);
+        dst = stoi(palabra);
+
+        CodigoPost codigoPost( codigoPostal, ciudad, estado, latitud, longitud, zonaHoraria, dst );
+        codigosPostales.push_back(codigoPost);
+
+    }
+
+    float minimoLongitud, maximoLongitud;
+    maximoLongitud = minimoLongitud = codigosPostales[0].get_longitud();
+    float minimoLatitud, maximoLatitud;
+    minimoLatitud = maximoLatitud = codigosPostales[0].get_latitud();
+    for (int j = 1; j < codigosPostales.size(); ++j) {
+        float latitud = codigosPostales[j].get_latitud();
+        float longitud = codigosPostales[j].get_longitud();
+
+        if ( latitud < minimoLatitud ) minimoLatitud = latitud;
+        else if ( latitud > maximoLatitud ) maximoLatitud = latitud;
+
+        if ( longitud < minimoLongitud ) minimoLongitud = longitud;
+        else if ( longitud > maximoLongitud ) maximoLongitud = longitud;
+    }
+
+    cout << "Minimo latitud: " << minimoLatitud << ", maximo latitud: " << maximoLatitud << endl;
+    cout << "Minimo longitud: " << minimoLongitud << ", maximo longitud: " << maximoLongitud << endl;
+
+    cout << "Prueba" << endl;
+    return 0;
+
 
 
 
